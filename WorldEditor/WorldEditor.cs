1bbe883ca172e923ecca4397ae91e7f79b44d478
DreTaX <dretax@gmail.hu>
2018-05-19 11:40:39 +0200

    Creating base conception.

---
+++ WorldEditor/WorldEditor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using RustBuster2016;
+using RustBuster2016.API;
+using RustBuster2016.API.Events;
+using Hooks = RustBuster2016.API.Hooks;
+
+namespace WorldEditor
+{
+    public class WorldEditor : RustBuster2016.API.RustBusterPlugin
+    {
+        private bool IsAdmin = false;
+        private bool Enabled = false;
+        public IniParser SavedData;
+        
+        public override string Name
+        {
+            get { return "WorldEditor"; }
+        }
+
+        public override string Author
+        {
+            get { return "DreTaX"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+
+        public override void DeInitialize()
+        {
+            IsAdmin = false;
+            Enabled = false;
+            RustBuster2016.API.Hooks.OnRustBusterClientPluginsLoaded -= Loaded;
+            RustBuster2016.API.Hooks.OnRustBusterClientChat -= OnRustBusterClientChat;
+        }
+
+        public override void Initialize()
+        {
+            if (!Directory.Exists(RustBuster2016.API.Hooks.GameDirectory + "\\RB_Data\\WorldEditor"))
+            {
+                Directory.CreateDirectory(RustBuster2016.API.Hooks.GameDirectory + "\\RB_Data\\WorldEditor");
+            }
+            if (!File.Exists(RustBuster2016.API.Hooks.GameDirectory + "\\RB_Data\\WorldEditor\\Objects.ini"))
+            {
+                File.Create(RustBuster2016.API.Hooks.GameDirectory + "\\RB_Data\\WorldEditor\\Objects.ini").Dispose();
+            }
+            SavedData = new IniParser(RustBuster2016.API.Hooks.GameDirectory + "\\RB_Data\\WorldEditor\\Objects.ini");
+            RustBuster2016.API.Hooks.OnRustBusterClientPluginsLoaded += Loaded;
+            RustBuster2016.API.Hooks.OnRustBusterClientChat += OnRustBusterClientChat;
+            
+            //todo: load each section to the world, add editable, and moving functions.
+        }
+
+        private void OnRustBusterClientChat(ChatEvent ce)
+        {
+            if (ce.ChatUI.textInput.Text.Contains("/worldedit"))
+            {
+                if (ce.ChatUI.textInput.Text == "/worldedit")
+                {
+                    Enabled = !Enabled;
+                    Rust.Notice.Inventory("", "WorldEdit Toggled: " + Enabled);
+                }
+                else
+                {
+                    string[] args = ce.ChatUI.textInput.Text.Split(new string[] { "/worldedit"}, StringSplitOptions.None)[1].Split(' ');
+                    if (args.Length > 0)
+                    {
+                        string arg = args[0];
+                        switch (arg)
+                        {
+                            case "editsection":
+                                //todo
+                                break;
+                            case "idunnowhat":
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Loaded()
+        {
+            IsAdmin = this.SendMessageToServer("worldeditadmin").Contains("yes");
+        }
+    }
+}
---
+++ WorldEditorServer/WorldEditorServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Fougerite;
+using RustBuster2016Server;
+
+namespace WorldEditorServer
+{
+    public class WorldEditorServer : Fougerite.Module
+    {
+        private bool _FoundRB;
+        
+        public override string Name
+        {
+            get { return "WorldEditorServer"; }
+        }
+
+        public override string Author
+        {
+            get { return "DreTaX"; }
+        }
+
+        public override string Description
+        {
+            get { return "WorldEditorServer"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+
+        public override void Initialize()
+        {
+            Hooks.OnModulesLoaded += OnModulesLoaded;
+        }
+
+        public override void DeInitialize()
+        {
+            Hooks.OnModulesLoaded -= OnModulesLoaded;
+            if (_FoundRB)
+            {
+                RustBuster2016Server.API.OnRustBusterUserMessage -= OnRustBusterUserMessage;
+            }
+        }
+
+        private void OnModulesLoaded()
+        {
+            foreach (var x in Fougerite.ModuleManager.Modules)
+            {
+                if (x.Plugin.Name.ToLower().Contains("rustbuster"))
+                {
+                    _FoundRB = true;
+                    RustBuster2016Server.API.OnRustBusterUserMessage += OnRustBusterUserMessage;
+                    break;
+                }
+            }
+        }
+
+        private void OnRustBusterUserMessage(API.RustBusterUserAPI user, Message msgc)
+        {
+            if (msgc.PluginSender == "WorldEditor")
+            {
+                if (msgc.MessageByClient == "worldeditadmin")
+                {
+                    msgc.ReturnMessage = user.Player.Admin ? "yes" : "no";
+                }
+            }
+        }
+    }
+}
