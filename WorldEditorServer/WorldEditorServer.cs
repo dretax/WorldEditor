b17b511d6734c718c8fd0deab27010471ce3ca51
dretax <dretax14@gmail.com>
2019-03-06 18:00:24 +0100

    Reverting back to https://github.com/dretax/WorldEditor/commit/9d765189c0119ccc38e51d58bcf9b08db200bd7e

--- WorldEditorServer/WorldEditorServer.cs
+++ WorldEditorServer/WorldEditorServer.cs
@@ -45,8 +45,8 @@ namespace WorldEditorServer
             }
             AssetPath = AssetPath + @Util.GetRootFolder() + "\\Save\\WorldEditorServer\\myasset.unity3d";
             
-            RustBuster2016Server.API.AddFileToDownload(new RBDownloadable("WorldEditor\\myasset.unity3d", Util.GetRootFolder() + "\\Save\\WorldEditorServer\\myasset.unity3d"));
-            RustBuster2016Server.API.AddFileToDownload(new RBDownloadable("WorldEditor\\ClientSideAssets.txt", Util.GetRootFolder() + "\\Save\\WorldEditorServer\\ClientSideAssets.txt"));
+            RustBuster2016Server.API.AddFileToDownload(new RBDownloadable("WorldEditor\\", Util.GetRootFolder() + "\\Save\\WorldEditorServer\\myasset.unity3d"));	            RustBuster2016Server.API.AddFileToDownload(new RBDownloadable("myasset.unity3d", "WorldEditor\\", Util.GetRootFolder() + "\\Save\\WorldEditorServer\\myasset.unity3d", "524857840D0C8CE921BC7E711E7CB9F931085231", false));
+            RustBuster2016Server.API.AddFileToDownload(new RBDownloadable("WorldEditor\\", Util.GetRootFolder() + "\\Save\\WorldEditorServer\\ClientSideAssets.txt"));
             
             MainHolder = new GameObject();
             Handler = MainHolder.AddComponent<LoadingHandler>();
